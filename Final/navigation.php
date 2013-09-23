80e5b375efa062ddab582a56c92b036d33255650
unknown <The_Beast@.(none)>
2013-09-23 10:58:33 -0400

    new

--- Final/navigation.php
+++ Final/navigation.php
@@ -1,9 +1,31 @@
-echo ' <ul class="nav navbar-nav">
-	        <li class="home"><a href="./">Home</a></li>
-	        <li class="contact"><a href="contact.php">Contact</a></li>
-	        <li class="links"><a href="links.php">Links</a></li>
-			<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown </a>
-				        
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+   		<div class="container">
+   			<div class="navbar-header">
+				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+			    	<span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			    </button>
+		      	<a class="navbar-brand" href="#">Playground</a>
+			</div>
+		
+			<div class="collapse navbar-collapse bs-navbar-collapse">
+	            <ul class="nav navbar-nav">
+	              <li class="home"><a href="./">Home</a></li>
+	              <li class="contact"><a href="contact.php">Contact</a></li>
+	              <li class="links"><a href="links.php">Links</a></li>
+					<li class="dropdown">
+				        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+				        <ul class="dropdown-menu">
+				          <li><a href="#">Action</a></li>
+				          <li><a href="#">Another action</a></li>
+				          <li><a href="#">Something else here</a></li>
+				          <li><a href="#">Separated link</a></li>
+				          <li><a href="#">One more separated link</a></li>
+				        </ul>
 				      </li>
-      			</ul>';
+      			</ul>
+				<p class="navbar-text pull-right">Signed in as <a href="#" class="navbar-link">Moshe Plotkin</a></p>
+			</div>
+		</div>
+	</div>
---
+++ Playground/scripts/main.js
@@ -0,0 +1,5 @@
+$function(){
+	$(".container").css(("margin.left": -1000)).animate(("margin.left": 0).2000 )
+	
+	
+});
