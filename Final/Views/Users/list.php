2672367b2477b30b7859784786a1215823e8174e
unknown <N02550439@.active.newpaltz.edu>
2013-10-10 16:14:49 -0400

    10/10

--- Final/Views/Users/list.php
+++ Final/Views/Users/list.php
@@ -1,4 +1,7 @@
+<link href="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/css/jquery.dataTables.min.css" type="ext/css" rel="stylesheet">
+
 <div class="containter">
+	
 	
 	<h2>Users</h2>
 	<table class="table">
